099cdcb73bc819328cc7820e6a4ea18b5a7050ff
surwaseonkar1512 <surwaseonkar12345@gmail.com>
2025-03-10 01:10:42 +0530

    "push"

--- Backend/templates/passwordUpdate.js
+++ Backend/templates/passwordUpdate.js
@@ -55,8 +55,7 @@ exports.passwordUpdated = (email, name) => {
     
     <body>
         <div class="container">
-            <a href="https://Yogahar -edtech-project.vercel.app"><img class="logo"
-                    src="https://i.ibb.co/7Xyj3PC/logo.png" alt="Yogahar  Logo"></a>
+   
             <div class="message">Password Update Confirmation</div>
             <div class="body">
                 <p>Hey ${name},</p>
@@ -66,7 +65,7 @@ exports.passwordUpdated = (email, name) => {
             </div>
             <div class="support">If you have any questions or need further assistance, please feel free to reach out to us
                 at
-                <a href="mailto:info@Yogahar .com">info@Yogahar .com</a>. We are here to help!
+                <a href="mailto:info@Yogahar.com">info@Yogahar.com</a>. We are here to help!
             </div>
         </div>
     </body>
